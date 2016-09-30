8eda9a5cbcffa53bd29d938374c4c17810f9594e
Ivan Filimonov <snake73w@gmail.com>
2016-09-30 21:22:54 +0700

    refactored get_online_friends, output_friends_to_console.

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -5,16 +5,6 @@ import time
 APP_ID = 5648564
 
 
-def get_vk_api(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-        scope='friends',
-    )
-    return vk.API(session)
-
-
 def get_user_login():
     return input('Enter your login: ')
 
@@ -24,19 +14,22 @@ def get_user_password():
 
 
 def get_online_friends(login, password):
-    api = get_vk_api(login, password)
-    return api.friends.getOnline()
+    session = vk.AuthSession(
+        app_id=APP_ID,
+        user_login=login,
+        user_password=password,
+        scope='friends',
+    )
+    api = vk.API(session)
+    friends_online_ids = api.friends.getOnline()
+    return api.users.get(user_ids=friends_online_ids)
 
 
 def output_friends_to_console(friends_online):
-    api = get_vk_api(login, password)
-
     print('Friends online:')
-    for friend_id in friends_online:
-        friend = api.users.get(user_ids=friend_id)[0]
+    for friend in friends_online:
         print(friend['first_name'], friend['last_name'])
-        # applications should not send more than three requests per second
-        time.sleep(0.35)
+
 
 if __name__ == '__main__':
     login = get_user_login()
